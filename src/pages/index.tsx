6a4b59dda5b030234ab42ed6371542485b6d1ac5
Samuel Newman <mozzius@protonmail.com>
2022-11-28 14:50:30 +0000

    create t3 app

---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.example.hello.useQuery({ text: "from tRPC" });
+
+  return (
+    <>
+      <Head>
+        <title>newman.christmas</title>
+        <meta name="description" content="merry christmas!!!" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+            newman.<span className="text-[hsl(280,100%,70%)]">christmas</span>
+          </h1>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
